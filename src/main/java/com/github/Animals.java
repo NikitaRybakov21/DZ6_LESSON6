c9b41a481080eff9f5bf395741f4ab138cd08840
NikitaRybakov21 <nikitarybakov044@gmail.com>
2021-07-08 15:08:52 +0300

    DZ6_LESSON6

--- src/main/java/com/github/Animals.java
+++ src/main/java/com/github/Animals.java
@@ -1,5 +1,19 @@
 package com.github;
 
 public class Animals {
+    protected String name;
+    protected String type;
 
+    public Animals(String name,String type){
+        this.name = name;
+        this.type = type;
+    }
+
+    public void run(int n){
+        System.out.println(this.name+" бежит "+n+" метров");
+    }
+
+    public void swim(int n){
+        System.out.println(this.name+" плывёт "+n+" метров");
+    }
 }
--- src/main/java/com/github/Cat.java
+++ src/main/java/com/github/Cat.java
@@ -2,4 +2,18 @@ package com.github;
 
 public class Cat extends Animals{
 
+    public Cat(String name, String type) {
+        super(name,type);
+    }
+
+    @Override
+    public void run(int n){
+        if(n<=200) System.out.println(this.name+" бежит "+n+" метров");
+        else System.out.println(this.name+" устал");
+    }
+
+    @Override
+    public void swim(int n){
+        System.out.println(this.name+" не умеет плавать");
+    }
 }
